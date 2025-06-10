a98e4fbb9045fdaf23c538982529fb3a5251bce6
Vladislav929 <159521490+Vladislav929@users.noreply.github.com>
2025-06-10 18:18:40 +0300

    Create home.php

---
+++ views/home.php
@@ -0,0 +1,4 @@
+<?php require_once 'partials/header.php'; ?>
+<h2>Добро пожаловать в наш мебельный салон!</h2>
+<p>Мы предлагаем широкий ассортимент качественной мебели по доступным ценам.</p>
+<?php require_once 'partials/footer.php'; ?>
